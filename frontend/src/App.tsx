a18cd02df8d89f6f96be3c5a4ce0e8090ccc9605
Gabrielacota <cota69572@gmail.com>
2025-05-22 10:06:02 -0300

    feat(frontend): Implementacao do componente de logo

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from './components/Logo/logo'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Logo tagline="Realce sua beleza com quem entende do assunto!" />
+
+        <h1>Bem-vindo ao Retock!</h1>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
