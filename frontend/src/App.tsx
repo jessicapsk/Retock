3a4f1963cec84011e7f04b631c4705e42268ccbb
Lucas Ramos de Oliveira <lucas.ramos@arapiraca.ufal.br>
2025-05-23 22:52:03 -0300

    muda npm install

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Opa</h1>
-        <p>Testando</p>
+        <p>Testandpoijijojojoj</p>
       </header>
     </div>
   );
