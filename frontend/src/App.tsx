dbd6619d559b2814c5b1ddaa67dc2a4a7acbc57d
Lucas Ramos de Oliveira <lucas.ramos@arapiraca.ufal.br>
2025-05-28 17:39:55 -0300

    mudar postgres, mudar react router dom, mudar app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
-import { Outlet } from "react-router-dom";
+import RegisterPage from "pages/RegisterPage"
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <Outlet />
+      <RegisterPage />
     </div>
   );
 }
